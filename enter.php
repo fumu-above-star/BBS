272778ef53910861369e94a1d79551d18acf4763
fumu-above-star <129039778+fumu-above-star@users.noreply.github.com>
2023-04-05 19:32:45 +0900

    Add files via upload

---
+++ enter.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+</head>
+<body>
+
+<div style="text-align: center">
+<a href="./login.php">ログイン(既にアカウントをお持ちの方)</a>
+<br>
+<br>
+<a href="./regist.php">会員登録(まだアカウントをお持ちでない方)</a>
+</div>
+</body>
+</html>
